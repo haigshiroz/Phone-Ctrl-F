61655c5e73bcf814889b8026c0540ce5a13fb0bb
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-01-02 18:31:46 -0500

    Established communication with Python script to read images
    Need to still be able to pass images but converting to base64 is breaking.

--- app/camera.tsx
+++ app/camera.tsx
@@ -5,14 +5,17 @@ import { CameraView, CameraType, useCameraPermissions, CameraCapturedPicture } f
 // import Tesseract from 'tesseract.js';
 // import TextRecognition from '@react-native-ml-kit/text-recognition';
 // import TesseractOcr, { LANG_ENGLISH, LEVEL_WORD } from 'react-native-tesseract-ocr';
-import MlkitOcr from 'react-native-mlkit-ocr'
+// import MlkitOcr from 'react-native-mlkit-ocr'
 // import {VisionCloudTextRecognizerModelType} from '@react-native-firebase/ml-vision';
+import RNFS from 'react-native-fs';
+import axios from 'axios'
 
 export default function CameraScreen() {
   const [ctrlFText, setCtrlFText] = useState('');
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions(); 
   const [photoURI, setPhotoURI] = useState<string>('https://reactnative.dev/img/tiny_logo.png') // No original photo
+  const [photo64, setPhoto64] = useState<string>() // No original photo
   const [cameraRef, setCameraRef] = useState<CameraView | null>(null); // No original camera ref
 
   const takePicture = async () => {
@@ -20,6 +23,7 @@ export default function CameraScreen() {
       const photo = await cameraRef.takePictureAsync();
       if (photo) {
           setPhotoURI(photo.uri);
+          setPhoto64(photo.base64);
           console.log(photo); // Displays the captured photo details
           readPicture();
       }
@@ -64,12 +68,12 @@ export default function CameraScreen() {
     //   console.log(err);
     // }
 
-    try {
-      const recognizedText = await MlkitOcr.detectFromFile(photoURI);
-      console.log(recognizedText);
-    } catch (err) {
-      console.log(err);
-    }
+    // try {
+    //   const recognizedText = await MlkitOcr.detectFromFile(photoURI);
+    //   console.log(recognizedText);
+    // } catch (err) {
+    //   console.log(err);
+    // }
 
     // try {
     //   const recognizedText = await FirebaseMLVision.apply()
@@ -77,6 +81,101 @@ export default function CameraScreen() {
     // } catch (err) {
     //   console.log(err);
     // }
+
+    // try {
+    //   // const base64Image = await RNFS.readFile(photoURI, 'base64');
+    //   const apiKey = '0a94851b7f7815f0436237ba3ee6230ccfc97e0a';
+    //   const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`;
+
+    //   // https://vision.googleapis.com/v1/images:annotate?key=0a94851b7f7815f0436237ba3ee6230ccfc97e0a
+
+    //   const requestData = {
+    //     requests: [
+    //       {
+    //         image: {
+    //           // content: base64Image,
+    //           content: photoURI,
+    //         },
+    //         features: [
+    //           { 
+    //             type: 'LABEL_DETECTION', 
+    //             maxResults: 5 
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   };
+
+    //   // const apiResponse = await axios.post(apiUrl, requestData);
+    //   const apiResponse = await axios.post(
+    //     apiUrl,
+    //     requestData,
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     }
+    //   );
+
+    //   const data = apiResponse.data.responses[0].fullTextAnnotation.text;
+    //   console.log(data)
+
+    // } catch (err) {
+    //   console.log(err);
+    // }
+    
+    try {
+      // const formData = new FormData();
+      // formData.append('image', {
+      //   uri: photoURI,
+      //   type: 'image/jpeg',
+      //   name: 'temp_poho.jpeg',
+      // });
+
+      // console.log("1");
+
+      // const response = await axios.post('https://hshiroz.pythonanywhere.com/', formData, {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data',
+      //   },
+      // });
+
+
+    // Send the image using Axios
+    // const data = {
+    //   name: "image",
+    //   image: photo64, // Send the base64 string as part of the payload
+    //   mime_type: 'image/jpeg', // Optionally include the MIME type
+    // }
+
+    const base64Image = await RNFS.readFile(photoURI, 'base64');
+    const data = {
+      data: base64Image,
+    }
+
+    // const response = await axios.post('https://hshiroz.pythonanywhere.com/', data, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // });
+
+    const response = await axios.post('https://hshiroz.pythonanywhere.com/', data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+      console.log(response.data );
+
+      // 192.168.1.218  Windows IP
+      // http://127.0.0.1:5000 Flask IP
+      // http://192.168.1.218:5000/api/ocr
+
+      // curl -X POST -F "image=@"C:\Users\shiro\Downloads\testimage.jpg"" https://hshiroz.pythonanywhere.com/
+
+    } catch (err) {
+      console.log(err);
+    }
     
   };
 
