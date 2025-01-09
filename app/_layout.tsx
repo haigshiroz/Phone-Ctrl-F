487e9d4595d74c10b3910435850c78f5adcd34dc
haigshiroz <114354276+haigshiroz@users.noreply.github.com>
2025-01-09 07:31:49 -0500

    Removed tabs at bottom of page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,9 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return <Stack />;
-// }
-
-import { Tabs } from 'expo-router';
+import { Slot } from 'expo-router';
 import React from 'react';
 
 export default function RootLayout() {
 
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-        }}
-      />
-    </Tabs>
+    <Slot />
   );
 }
